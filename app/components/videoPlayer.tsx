c285868ea04666533f20e4a8c4c050fea8eb7453
Sayed Hadi <sayood.sh@gmail.com>
2024-07-16 15:30:44 +0300

    Fixed All problems Render Problem Left

--- app/components/videoPlayer.tsx
+++ app/components/videoPlayer.tsx
@@ -1,33 +1,37 @@
-import { Play } from "next/font/google";
-import React, { useRef, useState } from "react";
-import ReactPlayer, { YouTubePlayerProps } from "react-player/youtube"; // Make sure to import the correct props type
+
+
+import React, { useEffect, useRef, useState } from "react";
+import ReactPlayer from "react-player/youtube"; 
 
 export default function VideoPlayer() {
-  const videoRef = useRef<ReactPlayer | null>(null);
+  const videoRef = useRef(false);
 
-  const handlePlay = () => {
-    if (videoRef.current) {
-      videoRef.current.seekTo(0); // Start playing from the beginning
-      videoRef.current.play();
-    }
+  const togglePlay = () => {
+        videoRef.current = true
+        console.log("Video playing:", videoRef.current);
   };
-
-  const handlePause = () => {
-    if (videoRef.current) {
-      videoRef.current.pause();
-    }
-  };
-  return (
-    <>
-      <div>
-        <ReactPlayer
-          ref={videoRef}
-          playing ={}
-          url="https://www.youtube.com/watch?v=Tn6-PIqc4UM&t"
-        />
-      </div>
-      <button onClick={handlePlay}>Play</button>
-      <button onClick={handlePause}>Pause</button>
-    </>
-  );
+  const togglePause = () => {
+    videoRef.current = false
+    console.log("Video playing:", videoRef.current);
 }
+
+return (
+
+  <>
+    <div>
+      
+      <ReactPlayer
+        ref={videoRef}
+        controls = {false}
+        playing = {videoRef}
+        url="https://www.youtube.com/watch?v=Tn6-PIqc4UM&t"
+      />
+    </div>
+    <button onClick={togglePlay}>Play</button>
+    <button onClick={togglePause}>Pause</button>
+  </>
+);
+};
+
+  
+
